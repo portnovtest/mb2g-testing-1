30c5d078b1b318a3e433842916282e131635d488
Phil Dolanov <phildolganov@yahoo.com>
2019-08-03 19:07:18 -0700

    added using build profile for different test environments

---
+++ src/test/java/JavaHelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+    @Test
+    void getHello(){
+        System.out.println("############");
+        System.out.println(System.getenv("TEST_HOST"));
+    }
+}
